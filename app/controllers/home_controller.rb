b3e441c4d011c037f45941fc82f1383a6eb7323a
umesh <umesh.riverbed@gmail.com>
2018-03-15 11:03:16 +0100

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,22 @@
+class HomeController < ApplicationController
+	ActionController::Parameters.permit_all_parameters = true
+	def index
+	end
+
+	def save_data
+		require 'csv'
+		base_compare = Hash.new
+		CSV.open("csv/finger_data.csv", "w") do |csv|
+			csv << ["stroke_id", "x", "y", "time"]
+			params[:finger_data].each do |key, value|
+				if !base_compare[value[:id]]
+					base_compare[value[:id]] = {x:value[:x].to_i, y:value[:y].to_i,time:value[:time].to_i}
+				end
+				diff_x = base_compare[value[:id]][:x] - value[:x].to_i
+				diff_y = base_compare[value[:id]][:y] - value[:y].to_i
+				diff_time = value[:time].to_i - base_compare[value[:id]][:time]
+				csv << [value[:id], diff_x,diff_y,diff_time]
+			end
+		end
+	end
+end
